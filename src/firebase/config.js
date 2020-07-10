ce58c60d92538439c03cc338a37b7be9b149e335
sakunchamikara <2017cs014@stu.ucsc.cmb.ac.lk>
2020-07-10 23:54:51 +0530

    set up firebase data and utility data

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,9 @@
 import Firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: '',
-  databaseUrl: '',
-  projectId: '',
-  appId: '',
+  apiKey: 'AIzaSyDQ-B0u98wry10O6GRfuZu-sDgZNpiXjSI',
+  databaseUrl: 'https://simplaechat-3c94a.firebaseio.com/',
+  projectId: 'simplaechat-3c94a',
+  appId: '1:72252170019:android:2f81207e342f4044c7f696',
 };
 export default Firebase.initializeApp(firebaseConfig);
