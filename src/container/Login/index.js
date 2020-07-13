e107b7af6e2014efd24fa0fe08d2f68e69b94e51
sakunchamikara <2017cs014@stu.ucsc.cmb.ac.lk>
2020-07-13 22:48:52 +0530

    create login component and add other styles and components

--- src/container/Login/index.js
+++ src/container/Login/index.js
@@ -1,10 +1,58 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useState} from 'react';
+import {Text, SafeAreaView, View} from 'react-native';
+import {Logo, InputField, RoundCornerButton} from '../../component';
+import {globalStyle, color} from '../../utility';
 
 const Login = ({navigation}) => {
+  const [credential, setCredentials] = useState({
+    email: '',
+    password: '',
+  });
+  const {email, password} = credential;
+  const handleOnChange = (name, value) => {
+    setCredentials({
+      ...credential,
+      [name]: value,
+    });
+  };
+  const onLoginPress = () => {
+    if (!email) {
+      alert('Email is required');
+    } else if (!password) {
+      alert('Password is required');
+    } else {
+      alert(JSON.stringify(credential));
+    }
+  };
+
   return (
-    <SafeAreaView>
-      <Text onPress={() => navigation.navigate('SignUp')}>Login</Text>
+    <SafeAreaView style={[globalStyle.flex1, {backgroundColor: color.BLACK}]}>
+      <View style={globalStyle.containerCentered}>
+        <Logo />
+      </View>
+      <View style={(globalStyle.flex2, globalStyle.sectionCentered)}>
+        <InputField
+          placeholder="Enter Email"
+          value={email}
+          onChangeText={(text) => handleOnChange('email', text)}
+        />
+        <InputField
+          placeholder="Enter Password"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(text) => handleOnChange('password', text)}
+        />
+        <RoundCornerButton title="Login" onPress={() => onLoginPress()} />
+        <Text
+          style={{
+            fontSize: 28,
+            fontWeight: 'bold',
+            color: color.LIGHT_GREEN,
+          }}
+          onPress={() => navigation.navigate('SignUp')}>
+          Sign Up
+        </Text>
+      </View>
     </SafeAreaView>
   );
 };
---
+++ src/network/index.js
@@ -0,0 +1,15 @@
+import LoginRequest from "./login";
+import SignUpRequest from "./signup";
+import { AddUser, UpdateUser } from "./user";
+import LogOutUser from "./logout";
+import { senderMsg, recieverMsg } from "./messeges";
+
+export {
+  LoginRequest,
+  SignUpRequest,
+  AddUser,
+  LogOutUser,
+  UpdateUser,
+  senderMsg,
+  recieverMsg,
+};
